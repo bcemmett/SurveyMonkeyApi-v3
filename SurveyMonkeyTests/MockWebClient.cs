68ff55f433f13216a748f3621356229de16fc01a
Ben Emmett <benemmett@gmail.com>
2016-09-13 00:05:58 +0100

    Mechanism for recording and playing requests

--- SurveyMonkeyTests/MockWebClient.cs
+++ SurveyMonkeyTests/MockWebClient.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Specialized;
+﻿using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
 using System.Net;
 using System.Text;
 using SurveyMonkey;
@@ -7,18 +9,53 @@ namespace SurveyMonkeyTests
 {
     class MockWebClient : IWebClient
     {
+        public List<MockWebClientRequest> Requests { get; set; }
+        public List<string> Responses { get; set; }
+        private int _nextResponseSequence;
+
+        public MockWebClient()
+        {
+            Headers = new WebHeaderCollection();
+            QueryString = new NameValueCollection();
+            Requests = new List<MockWebClientRequest>();
+            Responses = new List<string>();
+        }
+
+        private void RecordRequest()
+        {
+            Requests.Add(new MockWebClientRequest
+            {
+                Headers = Headers,
+                QueryString = QueryString
+            });
+        }
+
+        private string GetNextData()
+        {
+            var response = Responses.Skip(_nextResponseSequence).First();
+            _nextResponseSequence++;
+            return response;
+        }
+
+
+        #region Interface
+
         public WebHeaderCollection Headers { get; set; }
         public NameValueCollection QueryString { get; set; }
         public Encoding Encoding { get; set; }
 
         public string DownloadString(string url)
         {
-            throw new System.NotImplementedException();
+            RecordRequest();
+            return GetNextData();
         }
 
         public string UploadString(string url, string verb, string body)
         {
-            throw new System.NotImplementedException();
+            RecordRequest();
+            return GetNextData();
         }
+
+        #endregion
     }
 }
---
+++ SurveyMonkeyTests/MockWebClientRequest.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Specialized;
+using System.Net;
+
+namespace SurveyMonkeyTests
+{
+    class MockWebClientRequest
+    {
+        public WebHeaderCollection Headers { get; set; }
+        public NameValueCollection QueryString { get; set; }
+    }
+}
