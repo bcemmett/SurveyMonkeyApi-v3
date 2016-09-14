6ca9562ad1a86d9a5aa7327abc1982841d3a7099
Ben Emmett <benemmett@gmail.com>
2016-09-14 07:09:48 +0100

    Reorder some containers' properties

--- SurveyMonkey/Containers/Question.cs
+++ SurveyMonkey/Containers/Question.cs
@@ -7,17 +7,17 @@ namespace SurveyMonkey.Containers
     [JsonConverter(typeof(LaxPropertyNameJsonConverter))]
     public class Question
     {
+        public long Id { get; set; }
         public QuestionFamily Family { get; set; }
         public QuestionSubtype Subtype { get; set; }
-        public List<Headings> Headings { get; set; }
-        public bool ForcedRanking { get; set; }
-        public string Href { get; set; }
-        public long Id { get; set; }
         public int Position { get; set; }
         public Required Required { get; set; }
         public Sorting Sorting { get; set; }
         public Validation Validation { get; set; }
+        public List<Headings> Headings { get; set; }
         public bool Visible { get; set; }
+        public bool ForcedRanking { get; set; }
+        public string Href { get; set; }
         public QuestionAnswer Answers { get; set; }
     }
 }
--- SurveyMonkey/Containers/Survey.cs
+++ SurveyMonkey/Containers/Survey.cs
@@ -7,23 +7,23 @@ namespace SurveyMonkey.Containers
     [JsonConverter(typeof(LaxPropertyNameJsonConverter))]
     public class Survey
     {
+        public long Id { get; set; }
         public string Title { get; set; }
         public string Category { get; set; }
         public string Language { get; set; }
-        public int QuestionCount { get; set; }
         public int PageCount { get; set; }
+        public int QuestionCount { get; set; }
+        public int ResponseCount { get; set; }
         public DateTime DateCreated { get; set; }
         public DateTime DateModified { get; set; }
-        public long Id { get; set; }
-        public string Href { get; set; }
         public ButtonsText ButtonsText { get; set; }
-        public Dictionary<string, string> CustomVariables { get; set; }
+        public string Href { get; set; }
         public string Preview { get; set; }
         public string EditUrl { get; set; }
         public string CollectUrl { get; set; }
         public string AnalyzeUrl { get; set; }
         public string SummaryUrl { get; set; }
-        public int ResponseCount { get; set; }
+        public Dictionary<string, string> CustomVariables { get; set; }
         public List<Page> Pages { get; set; }
     }
 }
