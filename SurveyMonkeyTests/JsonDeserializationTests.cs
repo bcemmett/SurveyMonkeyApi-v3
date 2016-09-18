8fdf62a5d7326fc193c6ab91460a2af3a7244102
Ben Emmett <benemmett@gmail.com>
2016-09-18 13:01:44 +0100

    Move json deserialisation tests into one place

--- SurveyMonkeyTests/JsonDeserializationTests.cs
+++ SurveyMonkeyTests/JsonDeserializationTests.cs
@@ -2,14 +2,71 @@
 using System.Linq;
 using System.Reflection;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using NUnit.Framework;
 using SurveyMonkey;
 
 namespace SurveyMonkeyTests
 {
     [TestFixture]
-    public class ContainerDeserialisationTests
+    public class JsonDeserializationTests
     {
+        #region DateTimeDeserialization
+
+        [Test]
+        public void DateTimeExplitZeroOffsetInSummerTime()
+        {
+            string input = @"{""Timestamp"":""2016-07-05T12:10:20+00:00""}";
+            DeserializeAndTestDateTime(input, new DateTime(2016, 7, 5, 12, 10, 20, DateTimeKind.Utc));
+        }
+
+        [Test]
+        public void DateTimeExplitZeroOffsetInWinterTime()
+        {
+            string input = @"{""Timestamp"":""2016-01-05T12:10:20+00:00""}";
+            DeserializeAndTestDateTime(input, new DateTime(2016, 1, 5, 12, 10, 20, DateTimeKind.Utc));
+        }
+
+        [Test]
+        public void DateTimeNoOffsetInSummerTime()
+        {
+            string input = @"{""Timestamp"":""2016-07-05T12:10:20""}";
+            DeserializeAndTestDateTime(input, new DateTime(2016, 7, 5, 12, 10, 20, DateTimeKind.Utc));
+        }
+
+        [Test]
+        public void DateTimeNoOffsetInWinterTime()
+        {
+            string input = @"{""Timestamp"":""2016-01-05T12:10:20""}";
+            DeserializeAndTestDateTime(input, new DateTime(2016, 1, 5, 12, 10, 20, DateTimeKind.Utc));
+        }
+
+        [Test]
+        public void DateTimeExplicitOffsetInSummerTime()
+        {
+            string input = @"{""Timestamp"":""2016-07-05T15:10:20+03:00""}";
+            DeserializeAndTestDateTime(input, new DateTime(2016, 7, 5, 12, 10, 20, DateTimeKind.Utc));
+        }
+
+        [Test]
+        public void DateTimeExplicitOffsetInWinterTime()
+        {
+            string input = @"{""Timestamp"":""2016-01-05T15:10:20+03:00""}";
+            DeserializeAndTestDateTime(input, new DateTime(2016, 1, 5, 12, 10, 20, DateTimeKind.Utc));
+        }
+
+        private void DeserializeAndTestDateTime(string input, DateTime desiredResult)
+        {
+            var parsed = JObject.Parse(input);
+            var output = parsed.ToObject<JsonDateTimeTestsContainer>();
+            Assert.AreEqual(DateTimeKind.Utc, output.Timestamp.Value.Kind);
+            Assert.AreEqual(desiredResult, output.Timestamp);
+        }
+
+        #endregion
+
+        #region ObjectsHaveBeenWrittenToUseCorrectTypesAndConverters
+
         [Test]
         public void AllValueTypesAreMadeNullable()
         {
@@ -59,5 +116,13 @@ namespace SurveyMonkeyTests
                 Assert.AreEqual(typeof(LaxEnumJsonConverter), element.ConverterType, failMessage);
             }
         }
+
+#endregion
+    }
+
+    [JsonConverter(typeof(LaxPropertyNameJsonConverter))]
+    class JsonDateTimeTestsContainer
+    {
+        public DateTime? Timestamp { get; set; }
     }
 }
