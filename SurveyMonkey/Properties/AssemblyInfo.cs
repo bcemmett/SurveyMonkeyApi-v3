cb60aeb809ff5c8e6e8aef1b4a65172bda589c10
Ben Emmett <benemmett@gmail.com>
2016-09-30 19:39:01 +0100

    Add AssemblyInformationalVersion

--- SurveyMonkey/Properties/AssemblyInfo.cs
+++ SurveyMonkey/Properties/AssemblyInfo.cs
@@ -34,5 +34,6 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
 
 [assembly: InternalsVisibleTo("SurveyMonkeyTests, PublicKey=00240000048000009400000006020000002400005253413100040000010001005950447217c0b51774d98537acdd2246afef2eee315308acc30639dfee0dcbf928d485498d8cb671998124d967b06ef65314bd53466811d05de3a70e2d7591dbe5ab5b03b1693f0e0d42e6db4f8acf951d72f8eaf4acc925e2fc9e5533fa3315229bd2bbd92b48fab29da1ab22a184337b77b80025c4bab62b673e0310143ca3")]
