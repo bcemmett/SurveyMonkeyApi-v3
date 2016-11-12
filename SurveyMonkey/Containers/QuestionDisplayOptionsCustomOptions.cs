84e6895f77cef2044d59dbe53e1aafa0f41045c2
Ben Emmett <benemmett@gmail.com>
2016-11-12 12:29:09 +0000

    Add custom display option props based on sliders

--- SurveyMonkey/Containers/QuestionDisplayOptionsCustomOptions.cs
+++ SurveyMonkey/Containers/QuestionDisplayOptionsCustomOptions.cs
@@ -1,9 +1,13 @@
 ﻿using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace SurveyMonkey.Containers
 {
     [JsonConverter(typeof(TolerantJsonConverter))]
     public class QuestionDisplayOptionsCustomOptions
     {
+        public List<string> OptionSet { get; set; }
+        public int StartingPosition { get; set; }
+        public int StepSize { get; set; }
     }
 }
