0a42cd9012dfb2171945d935420b5ff47a81109c
Ben Emmett <benemmett@gmail.com>
2016-11-12 12:24:47 +0000

    Reorder properties

--- SurveyMonkey/Containers/QuestionDisplayOptions.cs
+++ SurveyMonkey/Containers/QuestionDisplayOptions.cs
@@ -5,12 +5,12 @@ namespace SurveyMonkey.Containers
     [JsonConverter(typeof(TolerantJsonConverter))]
     public class QuestionDisplayOptions
     {
+        public string LeftLabel { get; set; }
         public string MiddleLabel { get; set; }
-        public bool? ShowDisplayNumber { get; set; }
-        public string DisplaySubtype { get; set; }
         public string RightLabel { get; set; }
         public string DisplayType { get; set; }
-        public string LeftLabel { get; set; }
+        public string DisplaySubtype { get; set; }
+        public bool? ShowDisplayNumber { get; set; }
         public QuestionDisplayOptionsCustomOptions CustomOptions { get; set; }
     }
 }
