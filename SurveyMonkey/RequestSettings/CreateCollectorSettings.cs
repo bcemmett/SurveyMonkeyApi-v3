b56f3508179ae5657930c8af42d0acf83e6ab2f9
Ben Emmett <benemmett@gmail.com>
2016-11-13 15:48:49 +0000

    Implement all properties for collector settings

--- SurveyMonkey/RequestSettings/CreateCollectorSettings.cs
+++ SurveyMonkey/RequestSettings/CreateCollectorSettings.cs
@@ -1,14 +1,22 @@
-﻿namespace SurveyMonkey.RequestSettings
+﻿using System;
+using SurveyMonkey.Containers;
+
+namespace SurveyMonkey.RequestSettings
 {
     public class CreateCollectorSettings
     {
-        public enum TypeOption
-        {
-            Weblink,
-            Email
-        }
-
-        public TypeOption Type { get; set; }
+        public Collector.CollectorType Type { get; set; }
         public string Name { get; set; }
+        public string ThankYouMessage { get; set; }
+        public string DisqualificationMessage { get; set; }
+        public DateTime? CloseDate { get; set; }
+        public string ClosedPageMessage { get; set; }
+        public string RedirectUrl { get; set; }
+        public bool? DisplaySurveyResults { get; set; }
+        public Collector.EditResponseOption? EditResponseType { get; set; }
+        public Collector.AnonymousOption? AnonymousType { get; set; }
+        public bool? AllowMultipleResponses { get; set; }
+        public string Password { get; set; }
+        public string SenderEmail { get; set; }
     }
 }
