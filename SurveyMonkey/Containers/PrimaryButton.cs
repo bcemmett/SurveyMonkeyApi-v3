5a44911c9fca644c0eb7ecef8de42f13c2fa5e43
cocoon <cocoon@wolke7.net>
2019-02-22 21:59:45 +0100

    add missing Collector Types and properties

---
+++ SurveyMonkey/Containers/PrimaryButton.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace SurveyMonkey.Containers
+{
+    [JsonConverter(typeof(TolerantJsonConverter))]
+    public class PrimaryButton
+    {
+        public string BgColor { get; set; }
+        internal string Text { get; set; }
+        public string TextColor { get; set; }
+    }
+}
