1eb1d9758747df9a7d54ed6792ac763bdc873c7b
Jake Lardinois <Jake.Lardinois@gmail.com>
2020-09-03 11:06:06 -0500

    Leverage Settings Interface over Multiple API Constructors

---
+++ SurveyMonkey/ISurveyMonkeyApiSettings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SurveyMonkey
+{
+    public interface ISurveyMonkeyApiSettings
+    {
+        string ApiUrl { get; set; }
+        string ApiKey { get; set; }
+        string AccessToken { get; set; }
+        int? RateLimitDelay { get; set; }
+        int[] RetrySequence { get; set; }
+        IWebClient WebClient { get; set; }
+    }
+}
