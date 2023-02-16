cf1362ef9d75547d3072683f3677824bc4b48865
Ben Emmett <bcemmett@users.noreply.github.com>
2023-02-16 00:09:11 +0000

    Add (failing) tests that all public methods / properties are on the ISurveyMonkeyApi interface

---
+++ SurveyMonkeyTests/InterfaceCompletenessTests.cs
@@ -0,0 +1,73 @@
+﻿using NUnit.Framework;
+using SurveyMonkey;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace SurveyMonkeyTests
+{
+    [TestFixture]
+    public class InterfaceCompletenessTests
+    {
+        [Test]
+        public void AllPublicMethodsOnSurveyMonkeyApiAreOnInterface()
+        {
+            var allowedExceptions = new List<string>
+            {
+            };
+
+            var concreteMethods = GetMethods(typeof(SurveyMonkeyApi));
+            var interfaceMethods = GetMethods(typeof(ISurveyMonkeyApi));
+            var missing = concreteMethods.Where(c =>
+                !c.IsSpecialName
+                && !interfaceMethods.Any(i => i.Name == c.Name)
+                && !allowedExceptions.Contains(c.Name)
+                );
+
+            Assert.IsNotEmpty(concreteMethods);
+            Assert.IsNotEmpty(interfaceMethods);
+            Assert.IsEmpty(missing, String.Join(",", missing.Select(m => m.Name)));
+        }
+
+        [Test]
+        public void AllPublicPropertiesOnSurveyMonkeyApiAreOnInterface()
+        {
+            var allowedExceptions = new List<string>
+            {
+                typeof(SurveyMonkeyApi).GetProperty(nameof(SurveyMonkeyApi.Proxy)).Name
+            };
+
+            var concreteProperties = GetProperties(typeof(SurveyMonkeyApi));
+            var interfaceProperties = GetProperties(typeof(ISurveyMonkeyApi));
+            var missing = concreteProperties.Where(c =>
+                !interfaceProperties.Any(i => i.Name == c.Name)
+                && !allowedExceptions.Contains(c.Name)
+            );
+
+            Assert.IsNotEmpty(concreteProperties);
+            Assert.IsNotEmpty(interfaceProperties);
+            Assert.IsEmpty(missing, String.Join(",", missing.Select(m => m.Name)));
+        }
+
+        private IEnumerable<MethodInfo> GetMethods(Type type)
+        {
+            BindingFlags flags =    BindingFlags.Public |
+                                    BindingFlags.Static |
+                                    BindingFlags.Instance |
+                                    BindingFlags.DeclaredOnly;
+
+            return type.GetMethods(flags);
+        }
+
+        private IEnumerable<PropertyInfo> GetProperties(Type type)
+        {
+            BindingFlags flags =    BindingFlags.Public |
+                                    BindingFlags.Static |
+                                    BindingFlags.Instance |
+                                    BindingFlags.DeclaredOnly;
+
+            return type.GetProperties(flags);
+        }
+    }
+}
